b44ab84adba8486137f7b2973fbd81088b2e3b14
Alex Komoroske <jkomoros@gmail.com>
2023-09-04 16:17:39 -0700

    Prompt for openai key

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,8 @@
 export const getEnvironment = async () => {
+    //TODO: persist environment
+    const openai_api_key = prompt('What is your openai_api_key?');
+    if (!openai_api_key) throw new Error('No openai_api_key provided');
     return {
-        openai_api_key: 'YOUR_API_KEY_HERE'
+        openai_api_key
     };
 };
