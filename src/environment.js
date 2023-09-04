9b086f0f7f673b2cc6360d4dc713ad54c855d000
Alex Komoroske <jkomoros@gmail.com>
2023-09-04 16:21:21 -0700

    Persist openai_api_key in local storage.

--- src/environment.js
+++ src/environment.js
@@ -1,8 +1,18 @@
+const ENVIRONMENT_KEY = 'environment';
+
 export const getEnvironment = async () => {
-    //TODO: persist environment
-    const openai_api_key = prompt('What is your openai_api_key?');
-    if (!openai_api_key) throw new Error('No openai_api_key provided');
-    return {
-        openai_api_key
-    };
+
+    let saved = localStorage.getItem(ENVIRONMENT_KEY);
+
+    if (!saved) {
+        const openai_api_key = prompt('What is your openai_api_key?');
+        if (!openai_api_key) throw new Error('No openai_api_key provided');
+        const data = {
+            openai_api_key
+        };
+        saved = JSON.stringify(data);
+        localStorage.setItem(ENVIRONMENT_KEY, saved);
+    }
+
+    return JSON.parse(saved);
 };
