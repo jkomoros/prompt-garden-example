a59f0add0b7c71fac61e06b92974dd8347246b77
Alex Komoroske <jkomoros@gmail.com>
2023-09-04 16:25:43 -0700

    Update message about storing your key.

--- src/environment.js
+++ src/environment.js
@@ -5,7 +5,7 @@ export const getEnvironment = async () => {
     let saved = localStorage.getItem(ENVIRONMENT_KEY);
 
     if (!saved) {
-        const openai_api_key = prompt('What is your openai_api_key?');
+        const openai_api_key = prompt('What is your openai_api_key?\nThe key will be stored locally but not transmitted to any server other than openai.com');
         if (!openai_api_key) throw new Error('No openai_api_key provided');
         const data = {
             openai_api_key
