b1af6b7226c988ea4e3996a3fdc0ab68aadebc1f
Alex Komoroske <jkomoros@gmail.com>
2023-09-04 16:03:03 -0700

    Put in sample code from prompt-garden

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert('Hello, world');
+import {
+    Garden,
+    ProfileBrowser,
+} from 'prompt-garden';
+
+const env = {
+    openai_api_key: 'YOUR_API_KEY_HERE'
+};
+
+const main = async () => {
+    const profile = new ProfileBrowser();
+    const garden = new Garden(env, profile);
+    await garden.ensureSeedPacket('https://raw.githubusercontent.com/jkomoros/prompt-garden/main/seeds/example-import.json');
+    const seed = await garden.seed('favorite-things-limerick');
+    const result = await seed.grow();
+    alert(result);
+};
+
+main();
